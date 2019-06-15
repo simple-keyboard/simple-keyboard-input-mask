a93a9dd0422f85b41eb76ca9d542267e2afbf0a0
Francisco Hodge <hello@franciscohodge.com>
2019-06-14 23:51:50 -0400

    Tests update

--- tests/test.js
+++ tests/test.js
@@ -7,23 +7,6 @@ it('Keyboard renders without crashing', () => {
   div.className += "simple-keyboard";
   document.body.appendChild(div);
 
-  new Keyboard({
-    debug: true,
-    onChange: input => input,
-    onKeyPress: button => button,
-    inputMask: "(99) 9999-9999",
-    modules: [
-      SimpleKeyboardInputMask
-    ],
-  });
-});
-
-it('Keyboard disableButtonHold will work', () => {
-  const div = document.createElement('div');
-  
-  div.className += "simple-keyboard";
-  document.body.appendChild(div);
-
   let keyboard = new Keyboard({
     debug: true,
     onChange: input => input,
