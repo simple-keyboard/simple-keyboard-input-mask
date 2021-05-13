30d1a5bd7dea8a170eda83d158bcf299da3a820a
Francisco Hodge <hello@franciscohodge.com>
2021-05-13 07:25:13 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-input-mask v3.0.195
+ *   simple-keyboard-input-mask v3.0.196
  *   https://github.com/hodgef/simple-keyboard-input-mask
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
